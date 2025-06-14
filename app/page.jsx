2a353d703050eea543c87653fd8a4badb38e4105
Ayush Sharma <Ayushsharmaarps@gmail.com>
2025-06-14 23:23:06 +0530

    fixing images issue

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ import Desc from "./Components/Desc";
 import IPOCard from "./Components/IPO";
 import OHLC from "./Components/leftSidebar";
 import NiftyValueCard from "./Components/NiftyData";
-// import Bull from "../public/images/bull.jpg"
+import Bull from "../public/images/bull.png"
 
 export default function Page() {
   return (
@@ -17,10 +17,12 @@ export default function Page() {
       <main className=" grid py-4">
         <div className="flex gap-10 mx-auto ">
           {/* <OHLC /> */}
-          <img src="/images/bull.png" alt="Bull" className="w-64 h-64 mt-16" />
+          <img src="/images/bull.png" alt="Bull" className="w-64 h-60 mt-16" />
 
           <Charts />
           <img src="/images/bear.png" alt="Bear" className="w-64 h-64 mt-16" />
+          <img src={Bull.src} alt="Bull" className="w-32 h-auto" />
+
           <IPOCard />
         </div>
         <div className="min-w-7xl mx-20 mt-4 ">
